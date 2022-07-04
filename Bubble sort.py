b2c455f342b81f60431cd1a9f6fb003b8abdf00a
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-07-04 10:09:45 +0300

    Create Bubble sort.py

---
+++ Bubble sort.py
@@ -0,0 +1,31 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def countSwaps(a):
+    count = 0
+    for i in range(n):
+        for j in range(n - 1):
+            if a[j] > a[j+1]:
+                count = count + 1
+                a[j], a[j+1] = a[j+1], a[j]
+    message = "Array is sorted in {} swaps."
+    print(message.format(count))
+    firstElement = "First Element: {}"
+    print (firstElement.format(a[0]))
+    lastElement = "Last Element: {}"
+    print (lastElement.format(a[-1]))
+    
+    return 
+            
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)

86a7f030f0c14c9af71f31a044b665c9c313de5a
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-07-06 18:30:49 +0300

    sorting colors

---
+++ sortColors.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def sortColors(self, nums):
+        for i in range(len(nums)):
+            j = i
+            while j>0 and nums[j-1]> nums[j]:
+                nums[j-1], nums[j] = nums[j], nums[j-1] 
+                j = j-1
+        

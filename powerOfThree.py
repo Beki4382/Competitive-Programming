d8938cbe5c52872c2015aa829d9d797710ae1474
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-09-24 20:29:00 +0300

    Delete powerOfThree.py

--- powerOfThree.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isPowerOfThree(self, n):
-        if n == 1 or n == 3:
-            return True
-        if n < 3 or (n % 3) != 0:
-            return False
-        return self.isPowerOfThree(n/3)
-        
-        

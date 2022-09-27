1b206f43275b37678a91e99543967c4ae0ebf167
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-09-27 20:01:03 +0300

    Time: 823 ms (30.40%), Space: 18.9 MB (32.80%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        boats = 0
+        start, end = 0, len(people) -1
+        while start <= end:
+            if people[start] +people[end] <= limit:
+                start +=1
+            end -=1
+            boats +=1
+            
+        return boats
+                
+            
+            
+        

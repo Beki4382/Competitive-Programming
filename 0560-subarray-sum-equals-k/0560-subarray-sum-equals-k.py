eb6533e6ab6fe4975033326a03680b4a3ee40124
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-10-11 08:09:58 +0300

    Time: 366 ms (69.10%), Space: 15.9 MB (69.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def subarraySum(self, nums, k):
         d = {}
-        currSum = count = 0
+        acc = count = 0
         for num in nums:
-            currSum += num
-            if currSum == k:
-                count+=1
-            if currSum - k in d:
-                count += d[currSum-k]
-            if currSum in d:
-                d[currSum] +=1
+            acc += num
+            if acc == k:
+                count += 1
+            if acc - k in d:
+                count += d[acc-k]
+            if acc in d:
+                d[acc] += 1
             else:
-                d[currSum] = 1
+                d[acc] = 1
         return count
         
         

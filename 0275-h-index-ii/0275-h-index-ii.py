6f25e15c4539aa24e8fa6c60de859d61ec2c9db6
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-10-27 00:15:23 +0300

    Time: 134 ms (80.91%), Space: 18 MB (76.36%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hIndex(self, citations):
+        n = len(citations)
+        ans = 0
+        citations.sort()
+        for i,c in enumerate(citations):
+            if n-i <= c:
+                ans = max(ans, n-i)
+        return ans
+        

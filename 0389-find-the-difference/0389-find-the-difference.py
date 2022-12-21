03773ceaf4d89d5f9ef7dddc09be69258d537246
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-12-21 11:45:58 +0300

    Time: 61 ms (58.67%), Space: 13.9 MB (75.36%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = Counter(s)
+        t = Counter(t)
+        for char in t:
+            if char not in s or s[char] < t[char]:
+                return char
+            
+            

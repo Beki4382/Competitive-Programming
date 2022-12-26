ec39e76629473e4109837425029de917d2f1d712
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-12-26 21:15:08 +0300

    Update 1512-number-of-good-pairs.py

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def numIdenticalPairs(self, nums):
-        c = Counter(nums)
-        print(c)
+        
         result = 0
-        for num in c.keys():
-            if c[num] > 1:
-                result += c[num] * (c[num]-1)//2
+        count = Counter(nums)
+        
+        for i in count.values():
+            result += i * (i - 1) //2
+            
         return result
-                
-        
-        
         
         
         

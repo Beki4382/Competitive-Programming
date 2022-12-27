63d98e8ad67546b00b9497f468873f41bc76f6e7
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2022-12-27 19:23:01 +0300

    Set .union() Operation

---
+++ set.union.py
@@ -0,0 +1,13 @@
+engNews = int(input())
+engTakers = input().split()
+frenchNews = int(input())
+frenchTakers = input().split()
+
+count = frenchNews
+
+for val in engTakers:
+    if val not in frenchTakers:
+        count += 1
+
+print(count)
+         

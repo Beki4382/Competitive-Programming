50e65b111dad9f00eba769e7d16b2652b1fa999c
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-02-20 16:29:52 +0300

    Time: 65 ms (29.52%), Space: 17.5 MB (94.17%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,16 +7,35 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        node_set = set()
+        # method one ----> using two pointers(no need for extra space o(1)) 
         
-        curr = head
-        while curr:
-            if curr in node_set:
-                return True
+        slow, fast = head, head
+        
+    
+        while fast and fast.next:
             
-            node_set.add(curr)
-            curr = curr.next
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+                
+        
         return False
+     
+        
+        
+        # method two -----> using hash set to get seen nodes o(n)
+#         node_set = set()
+        
+#         curr = head
+#         while curr:
+#             if curr in node_set:
+#                 return True
+            
+#             node_set.add(curr)
+#             curr = curr.next
+#         return False
             
                 
         

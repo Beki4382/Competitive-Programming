de66e04a21f10ab85e64cf672cdb9264c76a0855
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-02-20 17:35:36 +0300

    Time: 45 ms (94.15%), Space: 17.4 MB (55.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,15 +7,35 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        node_set = set()
-        curr = head
+        #method one -----> solve O(1)
         
-        while curr:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
             
-            if curr in node_set:
-                return curr
-            node_set.add(curr)
-            curr = curr.next
-            
+            if slow == fast:
+                p1 = head
+                p2 = fast
+                while p1:
+                    if p1 == p2:
+                        return p1
+                    p1 = p1.next
+                    p2 = p2.next
+                    
         return 
+        # method two ----> solve using hashset O(n)
+        
+#         node_set = set()
+#         curr = head
+        
+#         while curr:
+            
+#             if curr in node_set:
+#                 return curr
+#             node_set.add(curr)
+#             curr = curr.next
+            
+#         return 
         

6c5021156498ab8dc1b6bcd64c45529e92826081
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-03-03 04:29:22 +0300

    Time: 81 ms (37.57%), Space: 14.4 MB (33.60%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,17 +1,19 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         
-        lengthOfTrip = [ 0 for _ in range(1001)]
-      
-        for trip, i, j in trips:
-            lengthOfTrip[i] += trip 
-            lengthOfTrip[j] -= trip 
-    
-        carLoad = 0
+        count_arr = [0] * 1001 # create array length of trips
         
-        for i in range( len(lengthOfTrip) ):
-            carLoad += lengthOfTrip[i] 
-            if carLoad > capacity: 
-                return False                
-        return True 
+        for trip in trips:
+            if trip[0] > capacity:
+                return False 
+            count_arr[ trip[1] ] += trip[0] 
+            count_arr[ trip[2] ] -= trip[0]
+            
+        for i in range( 1, len(count_arr) ):
+            count_arr[i] += count_arr[i - 1]
+            
+            if count_arr[i] > capacity:
+                return False
+        return True
         
+       

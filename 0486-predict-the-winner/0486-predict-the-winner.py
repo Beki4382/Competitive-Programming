14bc02fb3ed5fba5971147a7cc2da7b94d7c313e
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-03-15 09:19:10 +0300

    Time: 100 ms (35.01%), Space: 13.8 MB (75.57%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        def findWinner(start, end, score, turn):    
+            if start > end:
+                return score >= 0
+            
+            if turn:
+                return findWinner(start + 1, end, score + nums[start], False) or findWinner(start, end - 1, score + nums[end], False)
+            
+            else:
+                 return findWinner(start + 1, end, score - nums[start], True) and findWinner( start, end -1, score - nums[end], True)
+                
+            
+                
+        return findWinner(0, len(nums) - 1, 0, True)

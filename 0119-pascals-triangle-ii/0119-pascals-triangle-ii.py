299e407e662b74a75c663c1e07f15d9771a3de1d
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-04-20 05:25:22 +0300

    Time: 31 ms (71.67%), Space: 13.8 MB (93.55%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        
-        def recur(row_idx):
-            if row_idx == 0:
+        def pascal(n):
+            if n == 0:
                 return [1]
-            res = recur(row_idx -1)
-            curr = [res[0]]
+            if n == 1:
+                return [1,1]
+            n = n-1
+            res = pascal(n)
             
+            new_res = [1]
             for i in range(1, len(res)):
-                curr.append( res[i-1] + res[i] )
-                
-            curr.append(1)
-            
-            return curr
-        
-        return recur(rowIndex)
+                new_res.append(res[i] + res[i-1])
+            new_res.append(1)
+            return new_res
+        return pascal(rowIndex)

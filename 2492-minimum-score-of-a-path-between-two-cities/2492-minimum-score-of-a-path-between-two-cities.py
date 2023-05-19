cae725c81e1cf19fa7f4ac054325e4dec89c1886
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-05-19 17:35:48 +0300

    Time: 1816 ms (38.22%), Space: 61.1 MB (81.40%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,19 +1,36 @@
 class Solution:
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        root = [i for i in range(n+1)]
-
-        def find(a):
-            if root[a] == a:
-                return a
-            root[a] = find(root[a]) # path compression  
-            return root[a]
-
-        def union(a, b):
-            a = find(a)
-            b = find(b)
-            root[a] = b
-
-        for a, b, _ in roads:
-            union(a,b)
-
-        return min(d for a,b,d in roads if find(n) == find(a))
+        def __init__(self):
+            self.graph = {}
+            self.rank = []
+            self.minimum = []
+        
+        def find(self, node):
+            if self.parent[node] != node:
+                self.parent[node] = self.find(self.parent[node])
+            
+            return self.parent[node]
+            
+        def union(self, n1, n2, dis):
+            p1, p2 = self.find(n1), self.find(n2)
+            if self.rank[p2] > self.rank[p1]:
+                p1, p2 = p2, p1
+            self.parent[p2] = p1
+            self.rank[p1] += self.rank[p2]
+            self.minimum[p1] = min(self.minimum[p1], self.minimum[p2], dis)
+            
+                
+        def minScore(self, n: int, roads: List[List[int]]) -> int:
+            self.parent = {i : i for i in range(n) }
+            self.rank = [1 for i in range(n)]
+            self.minimum = [float("inf") for _ in range(n)]
+            for n1, n2, dis in roads:
+                self.union(n1 - 1, n2 - 1, dis)
+            
+            
+            return self.minimum[self.find(0)]
+            
+            
+            
+        
+        
+    

c4a44e4955cc239302fd6de83b886cf918d26da3
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-05-31 18:52:58 +0300

    Time: 41 ms (50.10%), Space: 16.2 MB (39.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,19 +1,36 @@
+# class Solution:
+#     d = {}
+#     def tribonacci(self, n: int) -> int:
+#         if n == 0:
+#             return 0
+#         if n == 1:
+#             return 1
+#         if n == 2:
+#             return 1
+
+#         if n in self.d:
+#             res = self.d[n]
+#         else:
+#             res = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+#             self.d[n] = res
+#         return res
+
 class Solution:
-    d = {}
     def tribonacci(self, n: int) -> int:
+        ans = [0] * (n + 1)
         if n == 0:
             return 0
-        if n == 1:
-            return 1
-        if n == 2:
+        elif n <= 2:
             return 1
 
-        if n in self.d:
-            res = self.d[n]
-        else:
-            res = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
-            self.d[n] = res
-        return res
+        ans[0] = 0
+        ans[1] = 1
+        ans[2] = 1
 
-        
+        for i in range(3, n + 1):
+            ans[i] = ans[i - 1] + ans[i - 2] + ans[i - 3]
+
+        return ans[n]
+
+              
         

2effc87d6c24cb0ca56f95c328b8323604a153e5
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-10-02 18:28:44 +0300

    Time: 35 ms (82.07%), Space: 16.3 MB (63.10%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
+        n = len(nums) -1
+        dp1 = [0] * n
+        dp2 = [0] * n
         
-        if n == 1:
-            return nums[-1]
-        if n == 2:
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
             return max(nums)
         
-        dp = [0] * (n -1)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        
-        for i in range(2, n - 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-            
-        
-        dp2 = [0] * (n - 1)
+        dp1[0] = nums[0]
         dp2[0] = nums[1]
+        
+        dp1[1] = max(nums[0], nums[1])
         dp2[1] = max(nums[1], nums[2])
+        for i in range(2,len(nums)-1):
+            dp1[i] = max(dp1[i-2]+nums[i], dp1[i-1])
         
-        for i in range(3, n):
-            dp2[i-1] = max(dp2[i - 2], dp2[i - 3] + nums[i])
-            
-        return max(dp[-1], dp2[-1])
+        for j in range(3,(len(nums))):
+            dp2[j-1] = max(dp2[j-3] + nums[j], dp2[j-2])
             
         
+        return max(dp1[-1], dp2[-1])

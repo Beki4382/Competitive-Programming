922d266916213ce7c474d7cd21ae564b71f63653
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-10-14 08:49:24 +0300

    Time: 34 ms (82.87%), Space: 16.1 MB (96.94%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -1,20 +1,25 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        hashmap={}
-        start=[0,0]
-        facing="N"
-        LA={"N":"W","W":"S","S":"E","E":"N"}
-        RC={"N":"E","E":"S","S":"W","W":"N"}
-        res=[]
-        for i in instructions:
-            if i=="G":
-                if facing=="N": start[1] += 1
-                elif facing=="E": start[0] += 1
-                elif facing=="W": start[0] -= 1
-                elif facing=="S": start[1] -= 1
-            elif i=="L":
-                facing=LA[facing]
-            elif i=="R":
-                facing=RC[facing]
-        if start==[0,0] or facing!="N": return True
-        else: return False
+        directions = {'N':(0,1), 'E':(1,0), 'S':(0,-1), 'W':(-1,0)}
+        
+        leftCh = {'N':'W', 'W':'S', 'S':'E', 'E':'N'}
+        rightCh = {'N':'E', 'E':'S', 'S':'W', 'W':'N'}
+        
+        start = [0,0]
+        facing = 'N'
+        for i in range(len(instructions)):
+            if instructions[i] == "G":
+                start[0] += directions[facing][0]
+                start[1] += directions[facing][1]
+         
+            elif instructions[i] == "L":
+                facing = leftCh[facing]
+                
+            else:
+                facing = rightCh[facing]
+      
+            
+        if facing != 'N' or start == [0,0]:
+            return True
+        else:
+            return False

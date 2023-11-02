34fc86b0d8ebd02380ec208aa669d10ab5564457
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-11-02 04:10:52 +0300

    Added solution for Evaluate Division

--- leet-code-solutions/evaluate-division.py
+++ leet-code-solutions/evaluate-division.py
@@ -2,42 +2,33 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         ans = []
         graph = defaultdict(list)
-        
+
         for i in range(len(equations)):
             st, en = equations[i][0], equations[i][1]
             wg = values[i]
             graph[st].append((en,wg))
-            graph[en].append((st,wg**-1))
-            
+            graph[en].append((st, wg ** -1))
+
         def dfs(root,val,en):
-            if root not in graph or en not in graph:
+            if not root in graph or en not in graph:
                 return -1
-                
+            
             if root == en:
                 return val
-            
+
             seen.add(root)
             for child in graph[root]:
                 if child[0] not in seen:
-                    res = dfs(child[0], val*child[1],en)
-                    if res!= -1:
-                        return res   
+                    res = dfs(child[0], val * child[1], en)
+                    if res != -1:
+                        return res
             return -1
-            
+
         for q in queries:
-            st, en = q[0],q[1]
-            seen = set()            
+            st,en = q[0], q[1]
+            seen = set()
             ans.append(dfs(st,1,en))
-        return ans 
-           
-            
-        
-               
-                
-                
-                
-            
-            
-            
+        return ans
+       
         
         
